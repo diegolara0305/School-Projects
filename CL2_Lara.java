4da98afb99834899bb43293f57552767a5ecd22b
diegolara0305 <diegolarao@icloud.com>
2024-06-12 13:19:52 -0600

    Update CL2_Lara.java

--- CL2_Lara.java
+++ CL2_Lara.java
@@ -1,11 +1,4 @@
-/* Diego Lara
-[CS1101] Comprehensive Lab 2
-This work is to be done individually. It is not permitted to. share, reproduce, or alter any part of this assignment for any purpose.
- Students are not permitted from sharing code, uploading this assignment online in any form, or viewing/receiving/ 
- modifying code written from anyone else.
- This assignment is part. of an academic course at The University of Texas at El Paso and
-a grade will be assigned for the work produced individually by the student.
-*/
+/* Diego Lara*/
 
 import java.util.Scanner;
 import java.util.Random;
