8b70f9659e4bd4a3d2143c648739164e7e658524
diegolara0305 <diegolarao@icloud.com>
2024-06-12 13:20:13 -0600

    Update CL3v2_Lara.java

--- CL3v2_Lara.java
+++ CL3v2_Lara.java
@@ -2,11 +2,7 @@ import java.util.Locale.Category;
 
 import javax.xml.transform.Templates;
 
-/* Diego Lara
-[CS1101] Comprehensive Lab 3
-This work is to be done individually. It is not permitted to. share, reproduce, or alter any part of this assignment for any purpose. Students are not permitted from sharing code, uploading this assignment online in any form, or viewing/receiving/ modifying code written from anyone else. This assignment is part. of an academic course at The University of Texas at El Paso and
-a grade will be assigned for the work produced individually by the student.
-*/
+/* Diego Lara*/
 
 public class CL3v2_Lara {
     public static void main(String[] args) {
